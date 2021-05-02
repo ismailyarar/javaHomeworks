4efdf86aae0d3182ab46fc324b89a557faac5367
ismya <ismyarar@gmail.com>
2021-05-02 18:49:38 +0300

    CourseManager update

--- odev2/src/com/company/CourseManager.java
+++ odev2/src/com/company/CourseManager.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class CourseManager {
     public void register(Course course){
-        System.out.println(course.courseName+" "+"Kursuna kayıt oldunuz");
+        System.out.println(course.courseName+" "+"kursu eklendi");
     }
 }
